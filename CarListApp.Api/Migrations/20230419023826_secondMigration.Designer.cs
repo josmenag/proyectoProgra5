8204148b23180813c504231c191252e52d9cf9a9
Jose Mendez <josmenag@gmail.com>
2023-04-18 20:43:04 -0600

    sqlserver

--- CarListApp.Api/Migrations/20230419023826_secondMigration.Designer.cs
+++ CarListApp.Api/Migrations/20230419023826_secondMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CarListApp.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarListApp.Api.Migrations
 {
     [DbContext(typeof(CarsInventoryDBContext))]
-    partial class CarsInventoryDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230419023826_secondMigration")]
+    partial class secondMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.4");
